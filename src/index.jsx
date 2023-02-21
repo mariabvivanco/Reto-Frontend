028a3916956b940c934afdfde0d3f289fd2d01eb
betty <mariab.vivanco@gmail.com>
2023-02-21 17:08:45 +0100

    Creando el url base y las peticiones a los endpoints del back

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import RoutesApp from './routes';
+import BasketService from './services/BasketService';
+import ProductsService from './services/ProductsService';
+
+export const basketServiceInstance = new BasketService();
+export const productsServiceInstance = new ProductsService();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/services/ProductsService.js
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+// Peticiones axios relacionadas con los productos
+
+import retoFrontAxios from '../api/retoFrontAxios';
+
+export default class ProductsService {
+  async getProducts() {
+    const response = await retoFrontAxios.get('/product');
+    return response.data;
+  }
+
+  async getProductsDetails(id) {
+    const response = await retoFrontAxios.get(`/product/:${id}`);
+    return response.data;
+  }
+}
